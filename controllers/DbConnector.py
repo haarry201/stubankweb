855d92dbdef4918ca602c10b47d3e4648dbe7b91
Jacob Scase <jacobscase@hotmail.com>
2020-12-04 16:29:40 +0000

    add db connector and connection to cloud database,

--- controllers/DbConnector.py
+++ controllers/DbConnector.py
@@ -1,9 +1,29 @@
 import mysql.connector
+from mysql.connector.constants import ClientFlag
+
+
 
 
 class DbConnector:
+    def getConfig(self):
+        config = {
+            'user': 'root',
+            'password': 'yD0sP7fqBcwi',
+            'host': '34.105.173.105',
+            'database': 'data',
+            'client_flags': [ClientFlag.SSL],
+            'ssl_ca': './certificates/server-ca.pem',
+            'ssl_cert': './certificates/client-cert.pem',
+            'ssl_key': './certificates/client-key.pem'
+        }
+        return config
+
     def getConn(self):
-        cnx = mysql.connector.connect(user='root', password='yD0sP7fqBcwi',
-                                      host='34.105.173.105',
-                                      database='data')
-        cnx.close()
+        # now we establish our connection
+        config = self.getConfig()
+        conn = mysql.connector.connect(**config)
+        return conn
+
+    def closeConn(self, conn):
+        conn.close
+        return
--- newWeb.py
+++ newWeb.py
@@ -1,6 +1,10 @@
 from flask import Flask, Blueprint
+from controllers.DbConnector import DbConnector
 
 simple_page = Blueprint('simple_page', __name__, template_folder='templates')
 @simple_page.route('/<page>')
 def show(page):
-    return 'Hello World!'
+    db_connector = DbConnector()
+    conn = db_connector.getConn()
+    db_connector.closeConn(conn)
+    return 'We Have Connected'
