4c04b7d9414d3c2b880f90b68cc068ea0fb5d185
Rhys Minchin <rhysminchin@hotmail.co.uk>
2020-12-08 13:30:23 +0000

    created register_page.py and register.html added blueprints for register functionality

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+from register_page import register_page
 from account_page import account_page
 
 app = Flask(__name__)
-app.register_blueprint(account_page)
+app.register_blueprint(register_page, url_prefix="/register.html")
+app.register_blueprint(account_page, url_prefix="/accounts.html")
+
 
 @app.route('/')
 def index_page():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
