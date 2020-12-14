db6045ee6c0082ae12e979e9a1e1508169abb96a
Jacob Scase <j.scase@ncl.ac.uk>
2020-12-14 10:08:02 +0000

    update css to be ie compatible and add 2fa class

---
+++ controllers/TwoFactorAuthentication.py
@@ -0,0 +1,11 @@
+import pyotp
+
+class TwoFactorAuthentication:
+    def get_random_secret_key(self):
+        return pyotp.random_base32()
+    def get_barcode_url(self, secret_key,users_email):
+        return pyotp.totp.TOTP(secret_key).provisioning_uri(name=users_email, issuer_name='StuBank')
+    def verify_users_code(self, users_code, secret_key):
+        totp = pyotp.TOTP(secret_key)
+        code = totp.verify(users_code)
+        return code
