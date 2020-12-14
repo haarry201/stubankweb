e617da08dc2f34914fa78c82f8ff1a1756426a87
Jacob Scase <j.scase@ncl.ac.uk>
2020-12-14 16:36:51 +0000

    add card classes and new page checks if any cards exist already

---
+++ classes/UserCard.py
@@ -0,0 +1,9 @@
+class UserCard:
+    def __init__(self, num, type, start_date, exp_date, desc):
+        self.num = num
+        self.type = type
+        self.start_date = start_date
+        self.exp_date = exp_date
+        self.desc = desc
+
+
