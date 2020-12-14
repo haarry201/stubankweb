0ef962fe1e5a3e243f97e2092b3a11c3e6167d49
Jacob Scase <j.scase@ncl.ac.uk>
2020-12-14 16:36:51 +0000

    add card classes and new page checks if any cards exist already

--- app.py
+++ app.py
@@ -2,16 +2,22 @@ from flask import Flask, render_template, request
 from register_page import register_page
 from account_page import account_page
 from login_page import login_page
+from admin_new_card import admin_new_card_page
 
 app = Flask(__name__)
 app.register_blueprint(login_page, url_prefix="/login.html")
 app.register_blueprint(register_page, url_prefix="/register.html")
 app.register_blueprint(account_page, url_prefix="/accounts.html")
+app.register_blueprint(admin_new_card_page, url_prefix="/new_card.html")
 
 
 @app.route('/')
 def index_page():
     return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error.html", msg = e)
 
 if __name__ == '__main__':
     app.run()
---
+++ classes/CardInfo.py
@@ -0,0 +1,4 @@
+class CardInfo:
+    def __init__(self, desc, type_id):
+        self.desc = desc
+        self.type_id = type_id
