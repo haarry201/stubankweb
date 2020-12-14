31ed0dc98f82e1a2091700e5ab94b83165bcb433
Jacob Scase <j.scase@ncl.ac.uk>
2020-12-14 17:10:55 +0000

    add basic add new card functionality with template and function

--- admin_new_card.py
+++ admin_new_card.py
@@ -15,9 +15,16 @@ def admin_new_card_func():
         db_connector = DbConnector()
         conn = db_connector.getConn()
         cursor = conn.cursor()
+        if request.method == "POST":
+            desc = request.form.get("description")
+            card = CardInfo.CardInfo(desc, "001")
+            query = "INSERT INTO CardInfo " \
+                    "VALUES(%s, %s)"
+            args = (card.type_id, card.desc)
+            cursor.execute(query, args)
+            conn.commit()
         query = ("SELECT * FROM CardInfo")
         cursor.execute(query)
-
         row = cursor.fetchall()  # fetches all rows of table
         print("here2")
         if row:
@@ -25,29 +32,16 @@ def admin_new_card_func():
             print(row)
             for item in row:
                 print(item)
-                card = CardInfo("This is a new card", "001")
+                card = CardInfo.CardInfo(item[1], item[0])
                 allcards.append(card)
                 # checks input data against stored data
                 print("here4")
-                if request.method == "POST":
-                    desc = request.form.get("Description")
-                    card = CardInfo(desc, "002")
-                    cursor.close()
-                    cursor = conn.cursor()
-                    query = "INSERT INTO CardInfo " \
-                            "VALUES(%s, %s)"
-                    args = (card.type_id,card.desc)
-                    cursor.execute(query, args)
-                    conn.commit()
-                    cursor.close()
-                    conn.close()
-                return render_template("new_card.html", all_cards=allcards)
-        else:
-            return render_template('new_card.html', all_cards=allcards)
+
+        conn.close()
+        return render_template('new_card.html', all_cards=allcards)
     except Error as e:
         print(e)
-        return render_template("error.html", msg="These login credentials do not match an existing user, please try "
-                                                 "again", src="login.html")
+        return render_template("error.html", msg="There seems to be an error adding a card, please try again or contact the system administrator", src="login.html")
     finally:
         cursor.close()
         conn.close()
