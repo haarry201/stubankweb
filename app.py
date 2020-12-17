c9e6c53142f04dd9e4a94e57cb22f092117d7883
Rhys Minchin <rhysminchin@hotmail.co.uk>
2020-12-17 13:27:15 +0000

    now clears session attribute when 'logout' button pressed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, session
 from register_page import register_page
 from account_page import account_page
 from login_page import login_page
@@ -8,7 +8,7 @@ secret_key = secrets.token_hex(16)
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = secret_key # generates secret key for unique session id
+app.config['SECRET_KEY'] = secret_key  # generates secret key for unique session id
 app.register_blueprint(login_page, url_prefix="/login.html")
 app.register_blueprint(register_page, url_prefix="/register.html")
 app.register_blueprint(account_page, url_prefix="/accounts.html")
@@ -16,6 +16,7 @@ app.register_blueprint(expenditure_reports, url_prefix="/reports.html")
 
 @app.route('/')
 def index_page():
+    session.pop('name')  # removes stored session attributes as logging out
     return render_template('index.html')
 
 if __name__ == '__main__':
