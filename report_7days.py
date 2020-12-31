18b1a62fc9d1d23ed52bb2d62c6a2cf5eaecd7d8
Rhys Minchin <rhysminchin@hotmail.co.uk>
2020-12-31 12:00:59 +0000

    -added functionality to get bar chart headings using dates of last 7 days -changed html files so all have a fixed navbar bottom to fix formatting issues

--- report_7days.py
+++ report_7days.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, session
 from controllers.DbConnector import DbConnector
+from datetime import date, timedelta
 
 report_7days = Blueprint('report_7days', __name__, template_folder='templates')
 
@@ -9,7 +10,17 @@ def reports_7days():
     db_connector = DbConnector()
     conn = db_connector.getConn()
     db_connector.closeConn(conn)
-    legend = 'Monthly Data'
-    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    cursor = conn.cursor(buffered=True)
+    cursor.execute("SELECT * FROM Transactions")  # gets all data stored in Transactions table
+
+    dates = []
+    today = date.today()
+    dates.append(str(today.day) + "/" + str(today.month))
+
+    for i in range(6):
+        today = today - timedelta(days=1)
+        dates.append(str(today.day) + "/" + str(today.month))
+
+    legend = 'Expenditure report for last 7 days'
     values = [10, 9, 8, 7, 6, 4, 7, 8]
-    return render_template('report_7days.html', values=values, labels=labels, legend=legend)
+    return render_template('report_7days.html', values=values, labels=dates, legend=legend)
