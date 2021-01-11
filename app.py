b1e5ee25314befaf927261b4e9c05f31037d8bc7
b9026661 <b9026661@ncl.ac.uk>
2021-01-11 17:29:36 +0000

    Finished template for money pools web page. Added skeleton for money pools code.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from manage_pools import manage_pools
 from register_page import register_page
 from account_page import account_page
 from login_page import login_page
@@ -9,6 +10,7 @@ app.register_blueprint(login_page, url_prefix="/login.html")
 app.register_blueprint(register_page, url_prefix="/register.html")
 app.register_blueprint(account_page, url_prefix="/accounts.html")
 app.register_blueprint(bank_acc_application_page, url_prefix="/bank_application.html")
+app.register_blueprint(manage_pools, url_prefix="/manage_pools.html")
 
 
 @app.route('/')
