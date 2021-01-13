45b1b4107942089cc2965fee5448459c4b4b515b
Jacob Scase <jacobscase@hotmail.com>
2021-01-13 10:27:29 +0000

    Add secret key to app py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from manage_pools import manage_pools
 from register_page import register_page
 from account_page import account_page
@@ -6,9 +6,10 @@ from login_page import login_page
 from bank_acc_application_page import bank_acc_application_page
 from bank_transfer_page import bank_transfer_page
 from offer_page import offer_page
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = secret_key  # generates secret key for unique session id
+app.config['SECRET_KEY'] = os.urandom(12).hex()  # generates secret key for unique session id
 app.register_blueprint(login_page, url_prefix="/login.html")
 app.register_blueprint(register_page, url_prefix="/register.html")
 app.register_blueprint(account_page, url_prefix="/accounts.html")
