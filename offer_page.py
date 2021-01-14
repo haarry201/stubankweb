758efda0148292abf189a598443edc212422de93
Jacob Scase <jacobscase@hotmail.com>
2021-01-14 16:21:12 +0000

    Finish implementing offers page, now all backend works, and displays offers

--- offer_page.py
+++ offer_page.py
@@ -1,5 +1,9 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request, session
 from controllers.DbConnector import DbConnector
+from mysql.connector import MySQLConnection, Error
+from controllers.DbConnector import DbConnector
+from controllers.Offer import Offer
+from datetime import datetime
 
 offer_page = Blueprint('offer_page', __name__, template_folder='templates')
 
@@ -8,6 +12,21 @@ offer_page = Blueprint('offer_page', __name__, template_folder='templates')
 def offer_page_func():
     db_connector = DbConnector()
     conn = db_connector.getConn()
-    db_connector.closeConn(conn)
-    user = {'username': 'Hello World'}
-    return render_template('offers.html', title='Home', user=user)
+    all_offers = []
+    try:
+        cursor = conn.cursor(buffered=True)
+        cursor.execute("SELECT * FROM Offers")  # gets all data stored in UserInfo table
+
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+            print(row[0])
+            new_offer = Offer(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+            all_offers.append(new_offer)
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+    return render_template('offers.html', title='Home', all_offers=all_offers)
