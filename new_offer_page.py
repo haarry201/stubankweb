1ae030d2a1f9b059fa47d10a98d52694c9b1c1cf
Jacob Scase <jacobscase@hotmail.com>
2021-01-14 16:14:08 +0000

    Finish implementing add new offer page

--- new_offer_page.py
+++ new_offer_page.py
@@ -7,31 +7,53 @@ from datetime import datetime
 new_offer_page = Blueprint('new_offer_page', __name__, template_folder='templates')
 
 
-@new_offer_page.route('/')
+@new_offer_page.route('/', methods=['GET', 'POST'])
 def new_offer_page_func():
     db_connector = DbConnector()
     conn = db_connector.getConn()
     if request.method == "POST":
-        print("hello world")
         offer_img_url = request.form.get("img-url")
         offer_origin_site = request.form.get("offer-origin-site")
         offer_title = request.form.get("offer-title")
-        offer_add_date = datetime.today().strftime('%d-%m-%Y-')
+        offer_add_date = datetime.today().strftime('%Y-%m-%d')
         offer_exp_date = request.form.get("offer-exp-date")
         offer_description = request.form.get("offer-description")
         offer_url = request.form.get("offer-url")
+        print(offer_description)
         new_offer = Offer(offer_description, offer_url, offer_img_url, offer_add_date, offer_exp_date,
                           offer_origin_site, offer_title)
         #Add new offer to DB
-    offer = Offer("Enter this code in the promotional code area during checkout in order to benefit from "
-                  "35% Off purchases made via the adidas App.",
-                  "https://www.myunidays.com/GB/en-GB/partners/adidas35/access/online",
-                  "https://images.unidays.world/i/18f8fbd4-b793-4e4c-9e84-b0c98cc0a136?format=raw",
-                  "10/12/2020",
-                  "20/12/2020",
-                  "Unidays",
-                  "35% Off"
-                  )
+        query = "INSERT INTO Offers(OfferDescription, OfferURL, OfferImageURL, OfferAddDate, OfferExpiryDate, OfferOriginSite, OfferTitle) " \
+                "VALUES(%s, %s, %s, %s, %s, %s, %s)"
+        args = (offer_description, offer_url, offer_img_url, offer_add_date, offer_exp_date, offer_origin_site, offer_title)
+
+        try:
+            db_connector = DbConnector()
+            conn = db_connector.getConn()
+            cursor = conn.cursor()
+            cursor.execute(query, args)
+            conn.commit()
+            cursor.close()
+        except Error as error:
+            print(error)
+            return render_template("error.html", msg="An unexpected error occurred, please try again",
+                                   src="register.html")
     all_offers = []
-    all_offers.append(offer)
+    try:
+        cursor = conn.cursor(buffered=True)
+        cursor.execute("SELECT * FROM Offers")  # gets all data stored in UserInfo table
+
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+            print(row[0])
+            new_offer = Offer(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+            all_offers.append(new_offer)
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
     return render_template('new_offer.html', title='Home', all_offers=all_offers)
