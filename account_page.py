fd33cd72d814b2120a36df59f5ce89a8099b14e8
Rhys Minchin <rhysminchin@hotmail.co.uk>
2021-01-19 12:21:49 +0000

    in the process of changing accounts.html to display actual transactions rather than hard-coded transactions. all data is ready, now need to iterate through data and display it on accounts.html

--- account_page.py
+++ account_page.py
@@ -1,15 +1,95 @@
 from flask import Flask, Blueprint, render_template, session, redirect, url_for
 from controllers.DbConnector import DbConnector
+import expenditure_reports
+from datetime import date, timedelta
 
 account_page = Blueprint('account_page', __name__, template_folder='templates')
 
 
 @account_page.route('/')
 def accounts_page():
-    db_connector = DbConnector()
-    conn = db_connector.getConn()
-    db_connector.closeConn(conn)
+    conn = expenditure_reports.get_conn()
+    accounts = expenditure_reports.get_info()  # uses previously written functionality to get all accounts owned by user
+    cursor = conn.cursor(buffered=True)
+    cursor.execute("SELECT * FROM UserAccounts")  # gets all data stored in UserAccounts table
+    row = cursor.fetchone()
+    savings_bal = 0; current_bal = 0
+    savings_acc = ''; current_acc = ''
+    while row is not None:
+        if str(row[0]) in accounts:
+            if str(row[3]) == '100':
+                savings_bal = float(row[5])  # if account is a 'savings' account, sets savings balance
+                savings_acc = str(row[0])
+            else:
+                current_bal = float(row[5])  # if account is a 'student' account, sets student balance
+                current_acc = str(row[0])
+        row = cursor.fetchone()
+    if savings_bal == 0:
+        savings_bal = 'N/A'
+    else:
+        savings_bal = format(savings_bal, '.2f')  # rounds to 2dp to display as amount of money
+
+    if current_bal == 0:
+        current_bal = 'N/A'
+    else:
+        current_bal = format(current_bal, '.2f')  # rounds to 2dp to display as amount of money
+
+    cursor.execute("SELECT * FROM Transactions")
+    row = cursor.fetchone()
+    transactions = []
+
+    today = date.today()
+    two_weeks = today - timedelta(days=14)  # gets date from 2 weeks ago as boundary for 'recent' transactions
+    t_day = str(today.day); w_day = str(two_weeks.day)
+    t_month = str(today.month); w_month = str(two_weeks.month)
+    if today.day < 10:
+        t_day = "0" + t_day
+    if today.month < 10:
+        t_month = "0" + t_month
+    if two_weeks.day < 10:
+        w_day = "0" + w_day
+    if two_weeks.month < 10:
+        w_month = "0" + w_month
+    today = str(today.year) + "-" + t_month + "-" + t_day
+    two_weeks = str(two_weeks.year) + "-" + w_month + "-" + w_day
+    b_year = today[0:4]; e_year = two_weeks[0:4]
+    b_month = today[5:7]; e_month = two_weeks[5:7]
+    b_day = today[8:10]; e_day = two_weeks[8:10]
+    start_boundary = date(int(b_year), int(b_month), int(b_day))
+    end_boundary = date(int(e_year), int(e_month), int(e_day))  # sets start and end boundaries as dates for comparison to database
+    while row is not None:
+        source = ''; t_type = ''
+        date_of_transaction = str(row[6])
+        year = date_of_transaction[0:4]
+        month = date_of_transaction[5:7]
+        day = date_of_transaction[8:10]
+        date_to_check = date(int(year), int(month), int(day))
+        print(str(start_boundary) + " " + str(date_to_check) + " " + str(end_boundary))
+        # recipient, account source, transaction type, amount, date
+        if str(row[1]) in accounts and (end_boundary <= date_to_check <= start_boundary):  # primary conditions to meet
+            recipient = str(row[10])
+            if str(row[1]) == current_acc:
+                source = "Current Account"
+            elif str(row[1]):
+                source = "Savings Account"
+
+            if "direct" in str(row[8]).lower():
+                t_type = "Direct Transfer"
+            elif "recurring" in str(row[8]).lower():
+                t_type = "Recurring Transaction"
+
+            amount = abs(row[5])
+            year = date_of_transaction[0:4]
+            month = date_of_transaction[5:7]
+            day = date_of_transaction[8:10]
+            transaction_date = day + "/" + month + "/" + year
+            transaction = [recipient, source, t_type, amount, transaction_date]
+            transactions.append(transaction)
+        row = cursor.fetchone()
+
+    print(transactions)
+
     if 'name' in session:
-        return render_template('accounts.html', title='Home', user=session['name'])
+        return render_template('accounts.html', title='Home', user=session['name'], savings=savings_bal, current=current_bal, transactions=transactions)
     else:
         return redirect(url_for('error_page.error_page_foo',code="e1", src="index.html"))
