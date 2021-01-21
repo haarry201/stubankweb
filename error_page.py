f281f71bc4af7aa9f1bb6b50d437ec796d11a63f
Jacob Scase <jacobscase@hotmail.com>
2021-01-21 12:12:23 +0000

    Update error page to check for error code and source, now doesnt crash server when none provided

--- error_page.py
+++ error_page.py
@@ -7,8 +7,14 @@ error_page = Blueprint('error_page', __name__, template_folder='templates')
 @error_page.route('/')
 def error_page_foo():
     print()
-    code = request.args['code']
-    src = request.args['src']
+    code = "e5"
+    src = "index.html"
+    try:
+        code = request.args['code']
+        src = request.args['src']
+    except:
+        print()
+
     error_codes = {"e1": "These login credentials do not match an existing user, please try again",
                    "e2": "An unexpected error occurred, please try again",
                    "e3": "Please ensure that all text boxes are filled in",
