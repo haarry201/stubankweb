83075a3c4601c2e952e00763be17fae1f5f27f3f
b9026661 <b9026661@ncl.ac.uk>
2021-01-27 03:53:19 +0000

    Finished majority of code behind manage_pools.py and display_pool.py

--- display_pool.py
+++ display_pool.py
@@ -1,12 +1,163 @@
-from flask import Flask, Blueprint, render_template,request
-from mysql.connector import MySQLConnection, Error
-from controllers.DbConnector import DbConnector
-from controllers.PasswordManager import PasswordManager
+import binascii
+import hashlib
+import os
+
+import mysql
+
+import expenditure_reports
+from flask import Blueprint, render_template, request, session
+from manage_pools import get_member_firstnames
 
 display_pool = Blueprint('display_pool', __name__, template_folder='templates')
 
 
 @display_pool.route('/', methods=['GET', 'POST'])
 def display_pool_func():
+    if not session.get("entered_pool_id"):
+        entered_pool_id = request.form.get("pool_to_view")
+        session["entered_pool_id"] = entered_pool_id
+    conn = expenditure_reports.get_conn()  # uses existing function for getting a db connection
+    cursor = conn.cursor(buffered=True)
+    cursor.execute("SELECT * FROM PoolAccounts")  # gets all data stored in UserPools table
+    row = cursor.fetchone()
 
-    return render_template('display_pool.html')
+    pool = []
+    while row is not None:
+        if row[0] == session['entered_pool_id']:
+            members_firstnames = get_member_firstnames(row[0])
+            member_ids = get_member_ids(row[0])
+            pool_balance = row[1]
+            pool_name = row[2]
+            owner = row[5]
+            date_created = row[6]
+            pool_join_code = row[7]
+            pool.append(pool_name + "," + session.get("entered_pool_id") + "," + str(
+                pool_balance) + "," + members_firstnames + "," + member_ids + "," + date_created + "," + owner)
+        row = cursor.fetchone()
+    if len(pool) == 0:
+        return render_template('register.html')
+    return render_template('display_pool.html', pool=pool, pool_name=pool_name, pool_join_code=pool_join_code)
+
+
+@display_pool.route('/deposit_money_pool', methods=['GET', 'POST'])
+def deposit_money_pool():
+    if request.method == "POST":
+        return render_template('display_pool.html')
+    return render_template('register.html')
+
+
+@display_pool.route('/withdraw_money_pool', methods=['GET', 'POST'])
+def withdraw_money_pool():
+    if request.method == "POST":
+        return render_template('display_pool.html')
+    return render_template('register.html')
+
+
+@display_pool.route('/leave_money_pool', methods=['GET', 'POST'])
+def leave_money_pool():
+    if request.method == "POST":
+        conn = expenditure_reports.get_conn()
+        cursor = conn.cursor()
+        query = "DELETE FROM UserPools WHERE UserID = %s AND PoolID = %s"
+        user_id = session.get("user_id")
+        pool_id = session.get("entered_pool_id")
+        cursor.execute(query, (user_id, pool_id))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return render_template('manage_pools.html')
+    return render_template('register.html')
+
+
+@display_pool.route('/add_user_to_money_pool', methods=['GET', 'POST'])
+def add_user_to_money_pool():
+    if request.method == "POST":
+        return render_template('display_pool.html')
+    return render_template('register.html')
+
+
+@display_pool.route('/remove_user_from_money_pool', methods=['GET', 'POST'])
+def remove_user_from_money_pool():
+    if request.method == "POST":
+        entered_pool_password = request.form.get("pool_password")
+        userid_to_remove = request.form.get("id_to_remove")
+        pool_id = session.get("entered_pool_id")
+        conn = expenditure_reports.get_conn()  # uses existing function for getting a db connection
+        cursor = conn.cursor(buffered=True)
+        cursor.execute("SELECT * FROM PoolAccounts")  # gets all data stored in UserPools table
+        row = cursor.fetchone()
+        while row is not None:
+            stored_password = row[3]
+            salt = row[4]
+
+            pool_pwdhash = hashlib.pbkdf2_hmac('sha512', entered_pool_password.encode('utf-8'), salt.encode('ascii'),
+                                               100000)
+            pool_pwdhash = binascii.hexlify(pool_pwdhash).decode('ascii')
+
+            if row[0] == pool_id and pool_pwdhash == stored_password:
+                cursor.close()
+                cursor = conn.cursor(buffered=True)
+                cursor.execute("SELECT * FROM UserPools")  # gets all data stored in UserPools table
+                row = cursor.fetchone()
+                while row is not None:
+                    if row[0] == pool_id and row[1] == userid_to_remove:
+                        cursor.close()
+                        cursor = conn.cursor()
+                        query = "DELETE FROM UserPools WHERE UserID = %s AND PoolID = %s"
+                        cursor.execute(query, (userid_to_remove, pool_id))
+                        conn.commit()
+                        cursor.close()
+                        conn.close()
+                    row = cursor.fetchone()
+                conn.close()
+            row = cursor.fetchone()
+
+        return render_template('accounts.html')
+    return render_template('register.html')
+
+
+@display_pool.route('/delete_money_pool', methods=['GET', 'POST'])
+def delete_money_pool():
+    if request.method == "POST":
+        pool_id = session.get("entered_pool_id")
+        try:
+            conn = expenditure_reports.get_conn()
+            conn.autocommit = False
+            cursor = conn.cursor()
+
+            query = "DELETE FROM UserPools WHERE PoolID = %s"
+            cursor.execute(query, (pool_id,))
+
+            query = "DELETE FROM PoolAccounts WHERE PoolID = %s"
+            cursor.execute(query, (pool_id,))
+
+            conn.commit()
+        except mysql.connector.Error as error:
+            print("Failed to delete pool: {}".format(error))
+            conn.rollback()
+        finally:
+            if conn.is_connected():
+                cursor.close()
+                conn.close()
+
+        return render_template('accounts.html')
+    return render_template('register.html')
+
+
+def get_member_ids(pool_id):
+    conn = expenditure_reports.get_conn()
+    cursor = conn.cursor(buffered=True)
+    cursor.execute("SELECT * FROM UserPools")  # gets all data stored in UserPools table
+    row = cursor.fetchone()
+
+    members_ids = ""
+    while row is not None:
+        if row[0] == pool_id:
+            if len(members_ids) == 0:
+                members_ids += row[1]
+            else:
+                members_ids += ", " + row[1]
+        row = cursor.fetchone()
+    cursor.close()
+    return members_ids
--- manage_pools.py
+++ manage_pools.py
@@ -1,8 +1,9 @@
+import binascii
 import hashlib
 import os
 import random
 import string
-import binascii
+from datetime import datetime
 
 import expenditure_reports
 from flask import Blueprint, render_template, session, request
@@ -12,11 +13,89 @@ manage_pools = Blueprint('manage_pools', __name__, template_folder='templates')
 
 @manage_pools.route('/', methods=['GET', 'POST'])
 def manage_pools_func():
-    return render_template('manage_pools.html')  # returns the html page
+    pools = []
+    pool_ids = get_pool_ids(get_user_id())
+
+    for pool_id in pool_ids:
+        conn = expenditure_reports.get_conn()
+        cursor = conn.cursor(buffered=True)
+        cursor.execute("SELECT * FROM PoolAccounts")  # gets all data stored in PoolAccounts table
+        row = cursor.fetchone()
+
+        while row is not None:
+            if row[0] == pool_id:
+                pool_name = row[2]
+                pool_balance = row[1]
+                pool_date = row[6]
+                pool_owner = row[5]
+                members_firstnames = get_member_firstnames(pool_id)
+
+                pools.append(pool_name + "," + pool_id + "," + str(pool_balance) + "," + members_firstnames + "," +
+                             pool_date + "," + pool_owner)
+            row = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+    return render_template('manage_pools.html', pools=pools)  # returns the html page
 
 
-def get_user_id():  # this code was reused from expenditure_reports.get_info()
-    user_id = ''
+@manage_pools.route('/create_money_pool', methods=['GET', 'POST'])
+def create_money_pool():
+    if request.method == "POST":
+        pool_id = ''.join(  # generate a random id for the pool to be used as the pk
+            random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+        pool_join_code = ''.join(  # generate a random id for the pool to be used as the join code
+            random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+        pool_name = request.form.get("pool_name")  # get values from the web page
+        pool_password = request.form.get("pool_password")
+
+        pool_salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')  # generate salt for password hashing
+        pool_pwd = hashlib.pbkdf2_hmac('sha512', pool_password.encode('utf-8'), pool_salt, 100000)
+        pool_pwdhash = binascii.hexlify(pool_pwd)  # hash password using salt, this is what is stored in database
+        pool_owner_name = session['name']
+        date = datetime.today().strftime('%d/%m/%Y')
+
+        pool_balance = 0
+        user_id = get_user_id()
+
+        query = "INSERT INTO PoolAccounts " \
+                "VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+        args = (pool_id, pool_balance, pool_name, pool_pwdhash, pool_salt, pool_owner_name, date, pool_join_code)
+        execute_query(query, args)
+
+        query = "INSERT INTO UserPools " \
+                "VALUES(%s, %s)"
+        args = (pool_id, user_id)
+        execute_query(query, args)
+
+        return render_template('accounts.html')
+    return render_template('register.html')
+
+
+@manage_pools.route('/join_money_pool', methods=['GET', 'POST'])
+def join_money_pool():
+    if request.method == "POST":
+        entered_join_id = request.form.get("join_code")  # get values from the web page
+        user_id = get_user_id()
+        conn = expenditure_reports.get_conn()  # uses existing function for getting a db connection
+        cursor = conn.cursor(buffered=True)
+        cursor.execute("SELECT * FROM PoolAccounts")  # gets all data stored in UserPools table
+        row = cursor.fetchone()
+
+        while row is not None:
+            if row[7] == entered_join_id:
+                pool_id = row[0]
+                query = "INSERT INTO UserPools " \
+                        "VALUES(%s, %s)"
+                args = (pool_id, user_id)
+                execute_query(query, args)
+            row = cursor.fetchone()
+        cursor.close()
+        return render_template('accounts.html')
+    return render_template('register.html')
+
+
+def get_user_id():
     conn = expenditure_reports.get_conn()
     cursor = conn.cursor(buffered=True)
     cursor.execute("SELECT * FROM UserInfo")  # gets all data stored in UserInfo table
@@ -24,53 +103,56 @@ def get_user_id():  # this code was reused from expenditure_reports.get_info()
     while row is not None:
         if row[5] == session['name']:
             user_id = row[0]
-    return user_id  # returns the user id of the current user
+            return user_id  # returns the user id of the current user
 
 
-@manage_pools.route('/create_money_pool', methods=['GET', 'POST'])
-def create_money_pool():
-    if request.method == "POST":
-        conn = expenditure_reports.get_conn()  # use existing function to get database connection
-        pool_id = ''.join(  # generate a random id for the pool to be used as the pk
-            random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
-        pool_name = request.form.get("pool_name")  # get values from the web page
-        pool_password = request.form.get("pool_password")
-
-        # generate random 16 digit hex to use as primary key for UserInfo table
-        otp_secret_key = random.randint(10000000, 99999999)  # generate one-time password
-        salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')  # generate salt for password hashing
-        pwd = hashlib.pbkdf2_hmac('sha512', pool_password.encode('utf-8'), salt, 100000)
-        pwdhash = binascii.hexlify(pwd)  # hash password using salt, this is what is stored in database
-
-        user_id = get_user_id()
-        account_nums = expenditure_reports.get_info()
-
-        #query = "INSERT INTO PoolAccounts " \
-                #"VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        #args = (pool_id, )
-        return render_template('manage_pools.html')
+def get_pool_ids(user_id):
+    pool_ids = []
+    conn = expenditure_reports.get_conn()
+    cursor = conn.cursor(buffered=True)
+    cursor.execute("SELECT * FROM UserPools")  # gets all data stored in UserInfo table
+    row = cursor.fetchone()
+    while row is not None:
+        if row[1] == user_id:
+            pool_ids.append(row[0])
+        row = cursor.fetchone()
+    return pool_ids  # returns all the pool id's for the user's pools
 
 
-def access_money_pool():
-    conn = expenditure_reports.get_conn()  # uses existing function for getting a db connection
-    return
+def get_member_firstnames(pool_id):
+    conn = expenditure_reports.get_conn()
+    cursor = conn.cursor(buffered=True)
+    cursor.execute("SELECT * FROM UserPools")  # gets all data stored in UserPools table
+    row = cursor.fetchone()
+
+    members_ids = []
+    while row is not None:
+        if row[0] == pool_id:
+            members_ids.append(row[1])
+        row = cursor.fetchone()
+    cursor.close()
+
+    for member_id in members_ids:
+        cursor = conn.cursor(buffered=True)
+        cursor.execute("SELECT * FROM UserInfo")  # gets all data stored in UserPools table
+        row = cursor.fetchone()
+
+        members_firstnames = ""
+        while row is not None:
+            if row[0] == member_id:
+                if len(members_firstnames) == 0:
+                    members_firstnames += row[5]
+                else:
+                    members_firstnames += ", " + row[5]
+            row = cursor.fetchone()
+        cursor.close()
+    return members_firstnames
 
 
-def leave_money_pool():
-    conn = expenditure_reports.get_conn()  # uses existing function for getting a db connection
-    return
-
-
-def deposit_to_money_pool():
-    conn = expenditure_reports.get_conn()  # uses existing function for getting a db connection
-    return
-
-
-def withdraw_from_money_pool():
-    conn = expenditure_reports.get_conn()  # uses existing function for getting a db connection
-    return
-
-
-def delete_money_pool():
-    conn = expenditure_reports.get_conn()  # uses existing function for getting a db connection
-    return
+def execute_query(query, args):
+    conn = expenditure_reports.get_conn()  # use existing function to get database connection
+    cursor = conn.cursor()
+    cursor.execute(query, args)
+    conn.commit()
+    cursor.close()
+    conn.close()
